95ef2696dc28ff717d17ed6c7ca80bf4dd7f2269
edguanuna <70737701+edguanuna@users.noreply.github.com>
2023-08-03 17:04:36 -0700

    DT Haas bot done

---
+++ app/api.cjs
@@ -0,0 +1,142 @@
+const puppeteer = require('puppeteer')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors({
+    origin: "http://localhost:3000"
+}))
+
+app.listen(3001, () => { console.log('Server is running on port 3001') });
+
+app.post('/api', (req, res) => {
+    console.log(req.body)
+    const { date, startTime, timeOfDay, duration, roomNumber, eventName, phoneNumber, calNetId, password } = req.body;
+    initBrowser(calNetId, password, date, startTime, timeOfDay, duration, eventName, phoneNumber, roomNumber)
+    res.send("Got the response")
+})
+
+
+const signInUrl = "https://ems.haas.berkeley.edu/emswebapp/Default.aspx?data=5BtDNigiZmYcBDtEfk61NA%3d%3d"
+
+function addMinutes(time, mins) {
+    // let timeParts = time.split(":");
+    let date = new Date(2024, 0, 1, time)
+    date.setHours(date.getHours() + 1)
+    return date.getHours() + ":" + (date.getMinutes()<10?'0':"") + date.getMinutes();
+    // let date = new Date(2023, 0, 1, timeParts[0], timeParts[1])
+    // console.log(date)
+    // date.setMinutes(date.getMinutes() + mins)
+    // return date.getHours() + ":" + (date.getMinutes()<10?'0':"") + date.getMinutes();
+}
+
+async function initBrowser(username, password, date, startTime, timeOfDay, duration, eventName, phoneNumber, roomNumber) {
+    const endTime = addMinutes(startTime, duration) + timeOfDay 
+    startTime = startTime + " " + timeOfDay
+
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+    await page.goto(signInUrl);
+
+    await loginScreen(page, username, password)
+    await runSearch(page, date, startTime, endTime)
+    await selectRoom(page, roomNumber)
+    await checkoutScreen(page, eventName, phoneNumber)
+
+    //Seal the deal.
+    await page.waitForSelector('.btn-success');
+    const searchButton = await page.$('.btn-success')
+    await page.evaluate((searchButton) => searchButton.click(), searchButton)
+
+}
+
+async function loginScreen(page, username, password) {
+    await page.type("#userID_input", username)
+    await page.type("#password_input", password)
+    await page.click("#pc_btnLogin")
+
+    await page.waitForSelector('a[href="#my-home"]');
+    const link = await page.$('a[href="#my-home"]');
+    await page.evaluate((link) => link.click(), link);
+
+    await page.waitForSelector('.btn-xs');
+    const buttons = await page.$$('.btn-xs')
+    const button = buttons[0]
+
+    await page.evaluate((button) => button.click(), button)
+}
+
+async function runSearch(page, date, startTime, endTime) {
+    await page.waitForSelector("#booking-date-input")
+    const dateInput = await page.$('input[id="booking-date-input"]');
+    const startTimeInput = await page.$('input[id="start-time-input"]');
+    const endTimeInput = await page.$('input[id="end-time-input"]');
+
+    //Clear date field
+    await page.evaluate((dateInput) => dateInput.click(), dateInput)
+    await new Promise(r => setTimeout(r, 400));
+    await page.type("#booking-date-input", "no")
+    await new Promise(r => setTimeout(r, 50));
+    for (let i=0; i<16; i++) {
+        await page.keyboard.press('Backspace')
+    }
+    await page.keyboard.type(date)
+    // await new Promise(r => setTimeout(r, 50));
+    // await page.type("#booking-date-input", "Thu 08/10/2023")
+
+    //Clear start time field
+    await page.evaluate((startTimeInput) => startTimeInput.click(), startTimeInput)
+    await new Promise(r => setTimeout(r, 300));
+    await page.type("#start-time-input", "")
+    await new Promise(r => setTimeout(r, 100));
+    for (let i=0; i<9; i++) {
+        await page.keyboard.press('Backspace')
+    }
+    await page.type("#start-time-input", startTime)
+    //Clear end time field
+    await page.evaluate((endTimeInput) => endTimeInput.click(), endTimeInput)
+    await new Promise(r => setTimeout(r, 300));
+    await page.type("#end-time-input", "")
+    await new Promise(r => setTimeout(r, 100));
+    for (let i=0; i<9; i++) {
+        await page.keyboard.press('Backspace')
+    }
+    await page.type("#end-time-input", endTime)
+    //HIt that search button
+    await page.waitForSelector('.find-a-room.btn-primary');
+    const searchButtons = await page.$$('.find-a-room.btn-primary')
+    const searchButton = searchButtons[0]
+    await page.evaluate((searchButton) => searchButton.click(), searchButton)
+}
+
+async function selectRoom(page, roomNumber) {
+    //convert room to data-room-id
+    await page.waitForSelector('div[data-room-id="' + roomNumber + '"]')
+    const div = await page.$('div[data-room-id="' + roomNumber + '"]');
+    const iTag = await div.$('i[tabindex="0"]');
+    await page.evaluate((iTag) => iTag.click(), iTag);
+    
+    await page.waitForSelector('a[href="#details"]')
+    const details = await page.$('a[href="#details"]');
+    await page.evaluate((details) => details.click(), details);
+
+    await page.waitForSelector('.btn-success');
+    const searchButton = await page.$('.btn-success')
+    await page.evaluate((searchButton) => searchButton.click(), searchButton)
+}
+
+async function checkoutScreen(page, eventName, phoneNumber) {
+    await page.keyboard.type(eventName)
+    await new Promise(r => setTimeout(r, 100));
+    // //Fill out phone number
+    // await page.waitForSelector('input[id="1stContactPhone1"]')
+    // const phoneNumberInput = await page.$('input[id="1stContactPhone1"]');
+    // await page.evaluate((phoneNumberInput) => phoneNumberInput.click(), phoneNumberInput)
+    await page.waitForSelector('.btn-success');
+    const searchButton = await page.$('.btn-success')
+    await page.evaluate((searchButton) => searchButton.click(), searchButton)
+
+    await page.keyboard.type(phoneNumber)
+    // //Seal the deal
+}
---
+++ app/components/form.js
@@ -0,0 +1,102 @@
+'use client'
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import InputMask from 'react-input-mask';
+
+
+export default function Form() {
+    const { register, handleSubmit } = useForm()
+    const [message, setMessage] = useState("HUFNDOO")
+
+
+    const onSubmit = async (data) => {
+        console.log(data)
+        const response = await fetch('http://localhost:3001/api', {
+            method: 'POST',
+            // mode: "no-cors",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+        if (response.ok) {
+            setMessage('Check your reservations to see if it worked! Lol');
+          } else {
+            setMessage(`Error: ${data.error}`);
+          }
+        console.log(response)
+    };
+
+
+    return (
+        <div className="min-h-screen font-sans antialiased flex flex-col items-center justify-top bg-gradient-to-r from-orange-400 via-gray-500 to-white">
+            <div className="text-center mt-6 mb-10">
+                <h2 className="text-2xl font-bold mb-5">HaasBot</h2>
+                <h1 className="text-4xl font-semibold">Welcome to DiversaTech's Haas Room Booker!</h1>
+            </div>
+            <div id="form" className="w-full max-w-xs">
+                <form onSubmit={handleSubmit(onSubmit)} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <div className="mb-4">
+                        <InputMask mask="99/99/2023" defaultValue="08/14/2023" {...register("date")} placeholder="MM/DD/YYYY" className="block appearance-none w-full bg-white border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" />
+                    </div>
+                    <div className="mb-6">
+                        <select {...register("startTime")} className="block appearance-none w-full bg-white border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                            <option value="10">10:00</option>
+                            <option value="11">11:00</option>
+                            <option value="12">12:00</option>
+                            <option value="1">1:00</option>
+                            <option value="2">2:00</option>
+                            <option value="3">3:00</option>
+                            <option value="4">4:00</option>
+                            <option value="5">5:00</option>
+                            <option value="6">6:00</option>
+                            <option value="7">7:00</option>
+                            <option value="8">8:00</option>
+                            <option value="9">9:00</option>
+                        </select>
+                    </div>
+                    <div className="mb-6">
+                        <select {...register("timeOfDay")} className="block appearance-none w-full bg-white border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                            <option value="AM">AM</option>
+                            <option value="PM">PM</option>
+                        </select>
+                    </div>
+                    <div className="mb-6">
+                        <select {...register("duration")} className="block appearance-none w-full bg-white border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                            <option value="sixty">60 min</option>
+                            <option disabled value="thirty">30 min</option>
+                        </select>
+                    </div>
+                    <div className="mb-6">
+                        <select {...register("roomNumber")} className="block appearance-none w-full bg-white border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                            <option value="341">N115</option>
+                            <option value="342">N150</option>
+                            <option value="326">N155</option>
+                            <option value="343">N158</option>
+                            <option value="345">N255</option>
+                            <option value="346">N258</option>
+                        </select>
+                    </div>
+                    <div className="mb-4">
+                    <input {...register("eventName")} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Event Name: DiversaTech"/>
+                    </div>
+                    <div className="mb-4">
+                    <input {...register("phoneNumber")} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Phone Number:"/>
+                    </div>
+                    <div className="mb-4">
+                    <input {...register("calNetId")} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Cal Net ID:"/>
+                    </div>
+                    <div className="mb-4">
+                    <input type="password" {...register("password")} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Password:"/>
+                    </div>
+                    <div className="flex items-center justify-between">
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                            Book Room
+                        </button>
+                    </div>
+                </form>
+                <p className='justify-center items-center'>{ message }</p>
+            </div>
+        </div>
+    );
+}
